917efb2c11461adf884331fe6c0adb0b13963a08
Joseph2303 <106033045+Joseph2303@users.noreply.github.com>
2025-09-04 23:52:34 -0600

    caso 2

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<script setup>
+import NavBar from '../components/NavBar.vue'   // 👈 coincide EXACTO con el nombre del archivo
+</script>
+
+<template>
+  <div>
+    <NavBar />
+    <NuxtPage />
+  </div>
+</template>
